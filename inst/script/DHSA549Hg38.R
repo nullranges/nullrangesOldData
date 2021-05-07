6fe876c6cc5d247cc800ab5599a3ac33e468fd40
Wancen <42752661+Wancen@users.noreply.github.com>
2021-05-06 22:04:56 -0400

    add DHSA549Hg38 documentation

--- inst/script/DHSA549Hg38.R
+++ inst/script/DHSA549Hg38.R
@@ -5,10 +5,10 @@ pks <- query(ah, c("DnaseSeq", "narrowPeak", "A549", "UniPk"))[[1]]
 pks <- keepStandardChromosomes(pks)
 seqlevels(pks) <- setdiff(seqlevels(pks), "chrM")
 hg19tohg38 <- import.chain("inst/extdata/hg19ToHg38.over.chain")
-peaks <- unlist(liftOver(pks, hg19tohg38))
-genome(peaks) <- "hg38"
-peaks <- sortSeqlevels(peaks)
-peaks <- sort(peaks)
-table(seqnames(peaks))
+DHSA549Hg38 <- unlist(liftOver(pks, hg19tohg38))
+genome(DHSA549Hg38) <- "hg38"
+DHSA549Hg38 <- sortSeqlevels(DHSA549Hg38)
+DHSA549Hg38 <- sort(DHSA549Hg38)
+table(seqnames(DHSA549Hg38))
 
-save(peaks, file = "data/DHSA549Hg38.rda")
+save(DHSA549Hg38, file = "data/DHSA549Hg38.rda")
